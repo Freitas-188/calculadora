d5d20cba22d00e32cbfd08767e202a4d753c699b
Freitas-188 <freitas14cdn@gmail.com>
2025-03-25 16:01:24 -0300

    first commit

---
+++ ADICAO.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* ESTE PROGRAMA TEM COMO OBJETIVO SOMAR DOIS NÚMEROS */
+
+	int main () {
+		
+	float x = 5.5;
+	float y = 10;
+	float calculo;
+	
+    calculo = x + y;
+	printf (" essa eh a soma %f", calculo);
+	
+	return 0;
+	
+	}
+
+
